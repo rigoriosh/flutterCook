3d2ba5c0ed2b4547186eb73f59001f8d61e1e678
rigoriosh <rigoriosh@gmail.com>
2020-06-18 18:08:28 -0500

    finalCoomit

--- lib/src/pages/basico_page.dart
+++ lib/src/pages/basico_page.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
 
 class BasicoPage extends StatelessWidget {
@@ -9,21 +11,39 @@ class BasicoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: <Widget>[
-          _imagen(),
-          _primeraFila(),
-          _segundaFila(),
-          _campoTexto()
-        ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            _imagen(),
+            _primeraFila(),
+            _segundaFila(),
+            _campoTexto(),
+            _campoTexto(),
+            _campoTexto(),
+            _campoTexto(),
+            _campoTexto(),
+            _campoTexto(),
+            _campoTexto(),
+            _campoTexto(),
+            _campoTexto(),
+            _campoTexto(),
+            _campoTexto(),
+          ],
+        ),
       ),
     );
   }
 
-  Image _imagen() {
-    return Image(
-        image: NetworkImage(
-            'https://www.xtrafondos.com/wallpapers/amanecer-anime-chica-silueta-paisaje-3721.jpg'));
+  Widget _imagen() {
+    return Container(
+      width: double.infinity,
+      child: Image(
+          image: NetworkImage(
+              'https://www.xtrafondos.com/wallpapers/amanecer-anime-chica-silueta-paisaje-3721.jpg'),
+              height: 350.0,
+              fit: BoxFit.cover,
+              ),
+    );
   }
 
   Container _primeraFila() {
@@ -65,39 +85,43 @@ class BasicoPage extends StatelessWidget {
 
   Widget _segundaFila() {
     return Row(
-       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      children: <Widget>[
-      _accion(Icons.call, 'CALL'),
-      _accion(Icons.near_me, 'ROUTE'),
-      _accion(Icons.share, 'SHARE'),            
-    ]
-    );
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+          _accion(Icons.call, 'CALL'),
+          _accion(Icons.near_me, 'ROUTE'),
+          _accion(Icons.share, 'SHARE'),
+        ]);
   }
 
   Widget _accion(IconData icono, String texto) {
     return Column(
-        children: <Widget>[
-          Icon(
-            icono,
-            color: Colors.blue,
-            size: 40.0,
-          ),
-          SizedBox(height: 10.0,),
-          Text(
-            texto,          
-            style: TextStyle(color: Colors.blue, fontSize: 15.0),
-          )
-        ],
-      );
+      children: <Widget>[
+        Icon(
+          icono,
+          color: Colors.blue,
+          size: 40.0,
+        ),
+        SizedBox(
+          height: 10.0,
+        ),
+        Text(
+          texto,
+          style: TextStyle(color: Colors.blue, fontSize: 15.0),
+        )
+      ],
+    );
   }
 
   Widget _campoTexto() {
-    return Container(
-      padding: EdgeInsets.all(20.0),
-      child: Text(
-        'Commodo sint est reprehenderit sunt non voluptate amet deserunt eu officia ipsum irure adipisicing. Lorem laborum ea labore deserunt veniam voluptate laboris consectetur Lorem sit dolore excepteur. Esse occaecat sint deserunt ad sunt.',
-        textAlign: TextAlign.justify,
+    return SafeArea(
+      child: Container(
+        //padding: EdgeInsets.all(20.0),
+        padding: EdgeInsets.symmetric(horizontal: 20.0),
+        child: Text(
+          'Commodo sint est reprehenderit sunt non voluptate amet deserunt eu officia ipsum irure adipisicing. Lorem laborum ea labore deserunt veniam voluptate laboris consectetur Lorem sit dolore excepteur. Esse occaecat sint deserunt ad sunt.',
+          textAlign: TextAlign.justify,
         ),
+      ),
     );
   }
 }
